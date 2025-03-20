9bd61b8b1d92ca8756d0a6a47971daed2009a1ae
Linus Winterhalder <l.winterhalder@zeroseven.de>
2025-03-20 12:52:50 +0100

    Add compatibility for TYPO3 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.4.99',
+            'typo3' => '11.5.0-13.4.99',
             'pagebased' => '*'
         ],
         'conflicts' => [
