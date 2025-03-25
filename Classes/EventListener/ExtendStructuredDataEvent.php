8bf83828686c218ed26876ffd088ee3e5742c3d3
Linus Winterhalder <l.winterhalder@zeroseven.de>
2025-03-25 14:15:08 +0100

    Fix Runtime Exception due to a caching issue in Frontend scope

--- Classes/EventListener/ExtendStructuredDataEvent.php
+++ Classes/EventListener/ExtendStructuredDataEvent.php
@@ -51,73 +51,80 @@ class ExtendStructuredDataEvent
     /** @throws ResourceDoesNotExistException */
     public function __invoke(StructuredDataEvent $event): void
     {
-        if (
-            ($registration = $event->getRegistration())
-            && ($uid = $event->getUid())
-            && ($event->getRegistration()->getExtensionName() === 'pagebased_blog')
-            && ($post = $registration->getObject()->getRepositoryClass()->findByUid($uid))
-        ) {
-            $event->addPropertyType('', [
-                '@context' => 'https://schema.org/',
-                'headline' => $post->getTitle(),
-                'url' => $this->forceAbsoluteUrl($post->getUid())
-            ], 'BlogPosting');
+        $registration = $event->getRegistration();
+        $uid = $event->getUid();
+        $extensionName = $registration?->getExtensionName();
 
-            if ($identifier = SettingsUtility::getExtensionConfiguration($registration, 'structuredData.identifier')) {
-                $event->addPropertyType('identifier', [
-                    'name' => $identifier,
-                    'value' => (string)$post->getUid()
-                ], 'PropertyValue');
-            } else {
-                $settings = SettingsUtility::getExtensionConfiguration($registration);
-                $settings['structuredData']['identifier'] = uniqid($registration->getExtensionName() . '-', false);
+        if ($extensionName !== 'pagebased_blog' || !$registration || !$uid) {
+            return;
+        }
 
-                GeneralUtility::makeInstance(ExtensionConfiguration::class)->set($registration->getExtensionName(), $settings);
-            }
+        // TODO: Keep an eye on issues in cached Frontend scope 
+        try {
+            $post = $registration->getObject()->getRepositoryClass()->findByUid($uid);
+        } catch (\Exception $e) {
+            return;
+        }
 
-            if ($image = $post->getFirstImage()) {
-                // @extensionScannerIgnoreLine
-                $event->addProperty('image', $image);
-            }
+        $event->addPropertyType('', [
+            '@context' => 'https://schema.org/',
+            'headline' => $post->getTitle(),
+            'url' => $this->forceAbsoluteUrl($post->getUid())
+        ], 'BlogPosting');
 
-            if ($description = $post->getDescription()) {
-                $event->addProperty('description', $description);
-            }
+        if ($identifier = SettingsUtility::getExtensionConfiguration($registration, 'structuredData.identifier')) {
+            $event->addPropertyType('identifier', [
+                'name' => $identifier,
+                'value' => (string)$post->getUid()
+            ], 'PropertyValue');
+        } else {
+            $settings = SettingsUtility::getExtensionConfiguration($registration);
+            $settings['structuredData']['identifier'] = uniqid($registration->getExtensionName() . '-', false);
+            GeneralUtility::makeInstance(ExtensionConfiguration::class)->set($registration->getExtensionName(), $settings);
+        }
 
-            if ($dateModified = $post->getLastChangeDate() ?? $post->getCreateDate()) {
-                $event->addProperty('dateModified', $dateModified->format('Y-m-d'));
-            }
+        if ($image = $post->getFirstImage()) {
+            // @extensionScannerIgnoreLine
+            $event->addProperty('image', $image);
+        }
 
-            if ($datePosted = ($post->getDate() ?? ($post->getAccessStartDate() ?? $post->getCreateDate()))) {
-                $event->addProperty('datePublished', $datePosted->format('Y-m-d'));
-            }
+        if ($description = $post->getDescription()) {
+            $event->addProperty('description', $description);
+        }
 
-            if ($author = $post->getContact()) {
-                $event->addPropertyType('author', [
-                    'name' => $author->getFullName(),
-                    'knowsAbout' => $author->getExpertise(),
-                    'image' => $author->getImage(),
-                    'sameAs' => [
-                        $this->forceAbsoluteUrl($author->getTwitter()),
-                        $this->forceAbsoluteUrl($author->getXing()),
-                        $this->forceAbsoluteUrl($author->getLinkedin()),
-                        $this->forceAbsoluteUrl($author->getPageLink())
-                    ]
-                ], 'Person');
-            }
+        if ($dateModified = $post->getLastChangeDate() ?? $post->getCreateDate()) {
+            $event->addProperty('dateModified', $dateModified->format('Y-m-d'));
+        }
 
-            if (($publisher = SettingsUtility::getPluginConfiguration($registration, 'settings.structuredData.publisher')) && !empty($publisher['name'])) {
-                $event->addPropertyType('publisher', [
-                    'name' => $publisher['name'],
-                    'url' => $publisher['url'] ?? $this->getRootPageUri($post->getUid()),
-                ], 'Organization');
+        if ($datePosted = ($post->getDate() ?? ($post->getAccessStartDate() ?? $post->getCreateDate()))) {
+            $event->addProperty('datePublished', $datePosted->format('Y-m-d'));
+        }
 
-                if ($logo = $publisher['logo'] ?? null) {
-                    if (file_exists($absolutePath = GeneralUtility::getFileAbsFileName($logo))) {
-                        $event->addPropertyType('publisher.logo', ['url' => $this->forceAbsoluteUrl(PathUtility::getAbsoluteWebPath($absolutePath))], 'ImageObject');
-                    } else {
-                        throw new ResourceDoesNotExistException('Creating structured data of the blog post failed. The organization logo ("' . $logo . '") does not exist.', 1689283412);
-                    }
+        if ($author = $post->getContact()) {
+            $event->addPropertyType('author', [
+                'name' => $author->getFullName(),
+                'knowsAbout' => $author->getExpertise(),
+                'image' => $author->getImage(),
+                'sameAs' => [
+                    $this->forceAbsoluteUrl($author->getTwitter()),
+                    $this->forceAbsoluteUrl($author->getXing()),
+                    $this->forceAbsoluteUrl($author->getLinkedin()),
+                    $this->forceAbsoluteUrl($author->getPageLink())
+                ]
+            ], 'Person');
+        }
+
+        if (($publisher = SettingsUtility::getPluginConfiguration($registration, 'settings.structuredData.publisher')) && !empty($publisher['name'])) {
+            $event->addPropertyType('publisher', [
+                'name' => $publisher['name'],
+                'url' => $publisher['url'] ?? $this->getRootPageUri($post->getUid()),
+            ], 'Organization');
+
+            if ($logo = $publisher['logo'] ?? null) {
+                if (file_exists($absolutePath = GeneralUtility::getFileAbsFileName($logo))) {
+                    $event->addPropertyType('publisher.logo', ['url' => $this->forceAbsoluteUrl(PathUtility::getAbsoluteWebPath($absolutePath))], 'ImageObject');
+                } else {
+                    throw new ResourceDoesNotExistException('Creating structured data of the blog post failed. The organization logo ("' . $logo . '") does not exist.', 1689283412);
                 }
             }
         }
